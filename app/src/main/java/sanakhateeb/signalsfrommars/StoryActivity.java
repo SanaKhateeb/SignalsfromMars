664881cff5b2614cd70d4ae9da6b6acc9e923a98
Sana <sanakhateeb14@hotmail.com>
2017-03-17 20:10:12 -0500

    second activity grabs data from first using string resources

--- app/src/main/java/sanakhateeb/signalsfrommars/StoryActivity.java
+++ app/src/main/java/sanakhateeb/signalsfrommars/StoryActivity.java
@@ -1,30 +1,22 @@
 package sanakhateeb.signalsfrommars;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 
 public class StoryActivity extends AppCompatActivity {
+    public static final String TAG = StoryActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+        Log.d(TAG, name);
+
     }
 
 }
